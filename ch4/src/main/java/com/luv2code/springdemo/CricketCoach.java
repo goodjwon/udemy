85352ec1773ead08660f882e3c3c01d625539ced
goodjwon <jwon76@naver.com>
2018-01-16 22:35:51 +0900

    5강 강좌 setter injection 및 setter 값 셋팅 하는 법 학습

--- ch4/src/main/java/com/luv2code/springdemo/CricketCoach.java
+++ ch4/src/main/java/com/luv2code/springdemo/CricketCoach.java
@@ -4,6 +4,11 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    //add new fields for emailAddress and team
+    private String emailAddress;
+    private String team;
+
+
     // creatre a no-arg constructor
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor ");
@@ -17,11 +22,33 @@ public class CricketCoach implements Coach{
 
     @Override
     public String getDailyWorkout() {
+
         return "Practice fast bowling for 15 minutes";
     }
 
     @Override
     public String getDailyFortune() {
+
         return fortuneService.getFortune();
     }
+
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoath: inside setter method - setEmailAddress: " + emailAddress);
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoath: inside setter method - setTeam: "+ team);
+        this.team = team;
+    }
+
 }
