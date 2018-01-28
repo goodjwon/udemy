92bcce646342a1db4af6a80bdaf5c71076537a3b
goodjwon <jwon76@naver.com>
2018-01-28 22:58:19 +0900

    section 8 - Autowired - Injection - IOC - annotation

--- ch7/src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ ch7/src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -2,15 +2,24 @@ package com.luv2code.springdemo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
+
+
 
     private FortuneService fortuneService;
 
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor.");
+    }
+
     @Autowired
-    public TennisCoach(FortuneService fortuneService) {
+    public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
 
