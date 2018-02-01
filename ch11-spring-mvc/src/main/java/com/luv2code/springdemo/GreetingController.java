29625dc73777d5abd08aba88dec07224a8c3252e
goodjwon <jwon76@naver.com>
2018-02-01 22:40:08 +0900

    section 11 - spring mvn use gradle - spring version 4.0.9 => 4.3.19

---
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/GreetingController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    public String greeting(
+            @RequestParam(value = "name"
+                , required = false
+                , defaultValue = "World")String name
+                , Model model) {
+
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+}
