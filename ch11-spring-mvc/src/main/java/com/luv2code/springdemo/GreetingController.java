c8ddb835fa8dac5882d2054ed074be651427e16b
goodjwon <jwon76@naver.com>
2018-02-01 22:42:44 +0900

    section 11 - controller annotation added

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/GreetingController.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/GreetingController.java
@@ -3,11 +3,13 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
+    @RequestMapping("/greeting")
     public String greeting(
             @RequestParam(value = "name"
                 , required = false
