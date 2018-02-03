a22bf26f08cbac9cb03e154b4c6fb78e1139374d
goodjwon <jwon76@naver.com>
2018-02-03 17:43:45 +0900

    section 11 - spring mvc

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/HelloWorldController.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 
 @Controller
 public class HelloWorldController {
@@ -23,5 +24,28 @@ public class HelloWorldController {
 
         return "helloworld";
     }
+
+    // new a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo!" + theName;
+
+        // add message to the model
+        model.addAttribute("name", theName);
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
 
 }
