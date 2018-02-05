0aafc4de178fd40a460306ad2f3dc70a470de0c9
goodjwon <jwon76@naver.com>
2018-02-05 21:06:31 +0900

    section 11 - spring mvc - @RequestParam Annotation Use Parents

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/HelloWorldController.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/HelloWorldController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     @RequestMapping("/showForm")
