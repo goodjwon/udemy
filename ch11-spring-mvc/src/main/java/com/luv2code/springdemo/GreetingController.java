267263df5232f3284bb302481136fa9f17a82d9c
goodjwon <jwon76@naver.com>
2018-02-07 23:07:37 +0900

    section 11 - spring mvc - @ModelAttribute - without thymeleaf tags - fixing view pages default spring tag

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/GreetingController.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/GreetingController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 public class GreetingController {
 
