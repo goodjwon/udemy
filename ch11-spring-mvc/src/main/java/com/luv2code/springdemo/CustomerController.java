6acc932198f05951fd83602b7409179c94764bfd
goodjwon <jwon76@naver.com>
2018-02-12 21:23:09 +0900

    section 15 - spring mvc - @Min - @Max - @InitBinder
    - 뷰를 구현 하지 않을 것이 기때문에 상관 없다.
    - thymeleaf tag 가 머리는 좀 아프지만 대충 해 보자.

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/CustomerController.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/CustomerController.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,14 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @InitBinder
+    public void intiBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor= new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
