f53f3eb9c277d2be11165a476b9627479d33766b
goodjwon <jwon76@naver.com>
2018-02-15 14:48:30 +0900

    section 15 - spring mvc - @Min - @Max - @InitBinder
    - make java annotation
    - use validation

--- ch11-spring-mvc/src/main/java/com/luv2code/springdemo/Customer.java
+++ ch11-spring-mvc/src/main/java/com/luv2code/springdemo/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo;
 
+import com.luv2code.springdemo.validation.CourseCode;
+
 import javax.validation.constraints.*;
 
 public class Customer {
@@ -17,6 +19,17 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getFirstName() {
         return firstName;
