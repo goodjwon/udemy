1b88db393e9c5b58405f2b97aa78c945dab2c575
goodjwon <jwon76@naver.com>
2018-03-19 21:23:08 +0900

    customer project start use spring data jpa

---
+++ web-customer-tracker/src/main/java/com/lov2code/springdemo/dao/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.lov2code.springdemo.dao;
+
+import com.lov2code.springdemo.entitiy.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+}
