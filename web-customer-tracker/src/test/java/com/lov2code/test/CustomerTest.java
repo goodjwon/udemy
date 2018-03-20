77548f9a3fc92e478944d5698a66c77ccb90bff0
goodjwon <jwon76@naver.com>
2018-03-21 07:59:42 +0900

    customer project start use spring data jpa spring data jpa test code added

--- web-customer-tracker/src/test/java/com/lov2code/test/CustomerTest.java
+++ web-customer-tracker/src/test/java/com/lov2code/test/CustomerTest.java
@@ -1,16 +1,26 @@
+package com.lov2code.test;
+
 import com.lov2code.springdemo.dao.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CustomerTest {
 
     @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
     private CustomerRepository repository;
+
 
     @Test
     public void testGetCustomers(){
