338428f1515bcf6e281bfb4d668ba466e78c48e9
goodjwon <jwon76@naver.com>
2018-03-20 21:38:00 +0900

    section 28~ - web-customer-tracker - crm done. use spring boot
    다음과제.
    - 스프링 데이터 jpa 테스트 케이스 만들어 보기.

--- web-customer-tracker/src/main/java/com/lov2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/lov2code/springdemo/controller/CustomerController.java
@@ -1,17 +1,30 @@
 package com.lov2code.springdemo.controller;
 
 
+import com.lov2code.springdemo.dao.CustomerRepository;
+import com.lov2code.springdemo.entitiy.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerRepository repository;
+
 
     @RequestMapping("/list")
     public String listCustomers(Model model) {
+
+        List<Customer> theCustomers = (List<Customer>) repository.findAll();
+
+        model.addAttribute("customer", theCustomers);
+
 
         return "list-customers";
     }
