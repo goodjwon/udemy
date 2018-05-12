0195a77d46ac0511159272cfcdd675393227997b
goodjwon <jwon76@naver.com>
2018-05-13 08:28:44 +0900

    add aspect  - aspect expression added  - any parameter  - any class, any method  - add method.  - use pointcut

--- aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,15 +3,17 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
+    @Pointcut("execution(public * com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDapPackage(){};
 
-
-    @Before("execution(public * com.luv2code.aopdemo.dao.*.*(..))")
+    @Before("forDapPackage()")
     public void beforeAddAccountAdivce() {
         System.out.println("\n====>>>> Executing @Before advice on addAccount()");
     }
