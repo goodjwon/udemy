20945b54877cd174cbbe6ad0dbfd6fadaf5ec135
goodjwon <jwon76@naver.com>
2018-06-02 11:33:08 +0900

    add aspect  - aspect expression added  - any parameter  - any class, any method  - add method.  - use pointcut  - reuse pointcut  - pointcut express  - add aspects  - aspects codding fixed exception  - add order

--- aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyCloudeLogAsyncAspect.java
+++ aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyCloudeLogAsyncAspect.java
@@ -3,10 +3,12 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudeLogAsyncAspect {
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDapPackageNoGetterAndSetter()")
--- aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aop-sample/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -5,10 +5,12 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDapPackageNoGetterAndSetter()")
